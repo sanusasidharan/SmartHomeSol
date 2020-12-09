d883d6b8082e4ffb1d5da90b3ba40695747221b4
Sanu_S <sanu_s@infosys.com>
2020-12-09 13:42:16 +0530

    Initial commit

---
+++ server.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify, request, send_file , send_from_directory
+import os
+import socket
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+PORT = int(os.getenv('PORT', 8000))
+# Change current directory to avoid exposure of control files
+#os.chdir('/static')
+host_name = socket.gethostname() 
+host_ip = socket.gethostbyname(host_name)
+        
+# driver function 
+if __name__ == '__main__': 
+    app.run(debug = True , host=host_ip, port=PORT )
